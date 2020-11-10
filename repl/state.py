422672948abd4f74aab9abe73e25d65d2c993a79
Kavi Gupta <kavi@berkeley.edu>
2020-11-09 21:12:13 -0800

    add some utility functions to the state

--- repl/state.py
+++ repl/state.py
@@ -1,14 +1,30 @@
 import attr
 
+from .specification import Spec
+
 
 @attr.s
 class State:
     partial_programs = attr.ib()
-    spec = attr.ib()
+    spec: Spec = attr.ib()
 
     @property
     def semantic_partial_programs(self):
         return [self.spec.partially_execute(p) for p in self.partial_programs]
+
+    @property
+    def done(self):
+        return any(self.spec.program_is_complete(p) for p in self.partial_programs)
+
+    @property
+    def is_goal(self):
+        return any(self.spec.program_is_correct(p) for p in self.partial_programs)
+
+    def transition(self, action):
+        return State(
+            [self.spec.program_class.production(action, self.partial_programs)],
+            self.spec,
+        )
 
 
 @attr.s
