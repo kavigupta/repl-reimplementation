c56897be51bd36d0f026490ba88e0e607b1cac9f
Kavi Gupta <kavi@berkeley.edu>
2020-11-09 21:17:46 -0800

    add rl finetuning

--- experiments/blocks_repl.py
+++ experiments/blocks_repl.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 from repl.blocks_environment import *
-from repl.train import pretrain
+from repl.train import pretrain, finetune
 
 
 config = BlockConfig()
-pretrain(BlocksPolicy(config), lambda rng: sample(config, rng), np.random)
+pretrain(BlocksPolicy(config), lambda rng: sample(config, rng), np.random, n=10 ** 5)
+finetune(
+    BlocksPolicy(config),
+    BlocksValue(config),
+    lambda rng: sample(config, rng),
+    np.random,
+)
--- repl/train.py
+++ repl/train.py
@@ -1,6 +1,7 @@
 from more_itertools import chunked
 
 import torch
+import numpy as np
 
 from .state import State
 
@@ -19,9 +20,49 @@ def pretrain(policy, sampler, rng, n=10000, lr=1e-3):
         log_probs = policy(states)
         _, predictions = log_probs.max(1)
         acc = (predictions.numpy() == actions).mean()
-        loss = -log_probs[range(log_probs.shape[0]), actions].mean()
+        loss = -log_probs[range(log_probs.shape[0]), actions].sum()
         if idx % 100 == 0:
             print("Idx", idx, "Accuracy:", acc, "Loss:", loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+
+def finetune_step(policy, value, sampler, rng, n=1000, lr=1e-3):
+    data = []
+
+    specs = [sampler(rng)[0] for _ in range(n)]
+    rewards = []
+    for idx, chunk in enumerate(chunked(specs, policy.batch_size)):
+        partials = policy.roll_forward(chunk)
+        for partial in partials:
+            last_state, _ = partial[-1]
+            reward = last_state.is_goal
+            rewards.append(reward)
+            for state, action in partial:
+                if action is None:
+                    continue
+                data.append((state, action, reward))
+    print(f"Rewards: {np.mean(rewards):.2f}")
+    rng.shuffle(data)
+
+    optimizer = torch.optim.Adam([*policy.parameters(), *value.parameters()], lr=lr)
+
+    for chunk in chunked(data, policy.batch_size):
+        states, actions, rewards = zip(*chunk)
+
+        v = value(states)
+        rewards = torch.tensor(rewards).float()
+        value_reward = (rewards * v.log() + (1 - rewards) * (1 - v).log()).sum()
+        log_probs = policy(states)
+        policy_reward = log_probs[range(log_probs.shape[0]), actions].sum()
+        loss = -(value_reward + policy_reward)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+def finetune(policy, value, sampler, rng, n=10000, **kwargs):
+    n_each = policy.batch_size * 10
+    for idxs in chunked(range(n), n_each):
+        finetune_step(policy, value, sampler, rng, **kwargs, n=len(idxs))
