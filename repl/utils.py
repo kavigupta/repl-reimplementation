913bbedc1e55c2a019191528cf2985cb1d20f0ff
Kavi Gupta <kavi@berkeley.edu>
2020-11-16 17:35:26 -0800

    add utils for saving

---
+++ repl/utils.py
@@ -0,0 +1,28 @@
+import os
+import torch
+
+
+def load_model(folder, step=None, architecture=lambda: None):
+    kwargs = {}
+    if not torch.cuda.is_available():
+        kwargs = dict(map_location=torch.device("cpu"))
+    if os.path.isfile(folder):
+        return None, torch.load(folder, **kwargs)
+    model_dir = os.path.join(folder, "model")
+    if not os.path.exists(model_dir):
+        return 0, architecture()
+    if step is None and os.listdir(model_dir):
+        step = max(os.listdir(model_dir), key=int)
+    path = os.path.join(model_dir, str(step))
+    if not os.path.exists(path):
+        return 0, architecture()
+    return int(step), torch.load(path, **kwargs)
+
+
+def save_model(model, folder, step):
+    path = os.path.join(folder, "model", str(step))
+    try:
+        os.makedirs(os.path.dirname(path))
+    except FileExistsError:
+        pass
+    torch.save(model, path)
