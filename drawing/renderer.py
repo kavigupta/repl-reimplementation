cf0b24b8dfca54124289eda1682806923d6f3ece
Kavi Gupta <kavi@berkeley.edu>
2020-12-16 21:51:58 -0800

    add stretch parameter to improve resolution

--- drawing/renderer.py
+++ drawing/renderer.py
@@ -14,11 +14,14 @@ def invert_transform(t, coordinates):
     return rescaled
 
 
-def render(items, size=(100, 100)):
+def render(items, size=(100, 100), stretch=10):
     w, h = size
-    x, y = np.meshgrid(np.arange(w) - w / 2, h / 2 - np.arange(h))
+    x, y = np.meshgrid(
+        np.arange(w * stretch) / stretch - w / 2,
+        h / 2 - np.arange(h * stretch) / stretch,
+    )
     coords = np.array([x, y])
-    image = np.zeros((*size, 3)) + 1
+    image = np.zeros((w * stretch, h * stretch, 3)) + 1
     for item in items:
         trans_coords = invert_transform(item.transform, coords)
         mask = item.type(*trans_coords)
