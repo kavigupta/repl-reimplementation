328350ed5aa7ddb641e7a3776f9d4fe5dbec0621
Kavi Gupta <kavi@berkeley.edu>
2020-12-27 16:51:18 -0800

    add the karel environment

---
+++ repl/karel_environment/load_karel_environment.py
@@ -0,0 +1,106 @@
+import os
+import pickle
+import struct
+
+from more_itertools import chunked
+
+import numpy as np
+
+from ..spec import Specification, Pair
+
+GRID_SIZE = (15, 18, 18)
+
+
+def standard_dataset(segment, *, path="data/karel_standard"):
+    if not os.path.exists(path):
+        error_message = f"""
+        Data not found. Run
+
+        mkdir -p {path}
+        cd {path}
+        wget https://s3.us-east-2.amazonaws.com/karel-dataset/karel.tar.gz
+        tar xf karel.tar.gz
+        cd -
+
+        """
+        raise RuntimeError(error_message)
+    root = os.path.join(path, "karel")
+    path_prefix = os.path.join(
+        root, {"train": "train", "test": "val"}[segment] + ".pkl"
+    )
+    return KarelData(path_prefix)
+
+
+def read_vocab(root="data/karel_standard"):
+    with open(os.path.join(root, "karel", "word.vocab")) as f:
+        vocab = [line.split() for line in f]
+    return {k: int(v) for k, v in vocab}
+
+
+class KarelData:
+    def __init__(self, path_prefix):
+        self.indices = read_index(path_prefix + ".index")
+        self.obj_file = open(path_prefix, "rb")
+
+    def shuffle(self, seed):
+        np.random.RandomState(seed).shuffle(self.indices)
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, i):
+        self.obj_file.seek(self.indices[i])
+        object = pickle.load(self.obj_file, encoding="latin-1")
+        pairs = [
+            Pair(get_one_hot(eg["in"]), get_one_hot(eg["out"]))
+            for eg in object["examples"]
+        ]
+        pairs = pairs[:-1]
+        return Specification(pairs), object["code"]
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+
+def get_one_hot(indices):
+    """
+    Input: a list of indices which refer to positions marked "True" in a Karel grid.
+    Output: a one-hot numpy array for the grid.
+    """
+    grid = np.zeros(GRID_SIZE, dtype=np.float32)
+    grid.ravel()[indices] = 1
+
+    return grid
+
+
+def read_index(filename):
+    index = []
+    with open(filename, "rb") as index_file:
+        while True:
+            offset = index_file.read(8)
+            if not offset:
+                break
+            (offset,) = struct.unpack("<Q", offset)
+            index.append(offset)
+    return index
+
+
+def batched_dataset_iter(*args, batch_size, **kwargs):
+    for batch in chunked(dataset_iter(*args, **kwargs), batch_size):
+        yield tuple(zip(*batch))
+
+
+def dataset_iter(segment, dataset=standard_dataset, seed=0):
+    """
+    Iterate through the dataset yielding spec, program pairs.
+
+    Arguments:
+        segment: the segment to use, must be 'train' or 'test'
+    """
+    ba = read_vocab()
+    dataset = dataset(segment)
+    dataset.shuffle(seed)
+    for spec, program in dataset:
+        program = [ba[tok] + 2 for tok in program]
+        yield spec, program
---
+++ repl/karel_environment/spec_encoder.py
@@ -0,0 +1,105 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+from ..lgrl import AttentionalSpecEncoder
+from ..utils import JaggedEmbeddings, PaddedSequence
+
+from .load_karel_environment import GRID_SIZE
+
+
+class KarelSpecEncoder(AttentionalSpecEncoder):
+    """
+    Rough implementation of a vision transformer: https://arxiv.org/pdf/2010.11929.pdf
+
+    Rather than dealing with multiple lengths, it just pads out extra variables with a special token
+
+    The positional embedding is added to the image tokens but not to the variable tokens, which
+        have no concept of position.
+
+    A transformer encoder is used to encode the input, and then another transformer is used
+        to autoregress over the previous sequential values and produce an output.
+
+    The result is then transformed into alphabet space and max-pooled.
+    """
+
+    def __init__(self, *, image_size=GRID_SIZE, embedding_size):
+        super().__init__(embedding_size)
+        self.e = embedding_size
+        self.encoder = KarelTaskEncoder((image_size[0] * 2, *image_size[1:]))
+
+    def encode(self, specifications):
+        flat_specs = []
+        indices = []
+        for spec in specifications:
+            indices.append(
+                list(range(len(flat_specs), len(flat_specs) + len(spec.pairs)))
+            )
+            flat_specs += spec.pairs
+
+        inputs = torch.tensor([fs.input for fs in flat_specs])
+        outputs = torch.tensor([fs.output for fs in flat_specs])
+        flat_specs = self.encoder(inputs, outputs)
+        src_tgt = flat_specs.transpose(0, 1)
+        # see entire_sequence_forward for note on mask
+        encoding = self.encode_attn(src_tgt, src_tgt)
+        encoding = encoding.transpose(0, 1)
+        mask = torch.ones(encoding.shape[:-1], dtype=torch.bool)
+        return JaggedEmbeddings(encoding, indices), mask
+
+
+class KarelTaskEncoder(nn.Module):
+    """Implements the encoder from:
+
+    Leveraging Grammar and Reinforcement Learning for Neural Program Synthesis
+    https://openreview.net/forum?id=H1Xw62kRZ
+
+    Instead of embedding the data, it just serializes it instead.
+    """
+
+    def __init__(self, image_size):
+        super().__init__()
+
+        c, w, h = image_size
+
+        self.image_size = image_size
+
+        self.input_encoder = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+        self.output_encoder = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+
+    def forward(self, input_grid, output_grid):
+        batch_dims = input_grid.shape[:-3]
+        input_grid = input_grid.contiguous().view(-1, *self.image_size)
+        output_grid = output_grid.contiguous().view(-1, *self.image_size)
+
+        input_enc = self.input_encoder(input_grid)
+        output_enc = self.output_encoder(output_grid)
+        enc = torch.cat([input_enc, output_enc], 1)
+        enc = enc + self.block_1(enc)
+        enc = enc + self.block_2(enc)
+
+        enc = enc.view(*(batch_dims + (-1, 64)))
+        return enc
