a5c840f11f46258205b68f5c69ca7de4ce118ea0
Kavi Gupta <kavi@berkeley.edu>
2020-12-31 00:31:40 -0800

    add search using permanant cache

--- repl/search/beam_search.py
+++ repl/search/beam_search.py
@@ -1,38 +1,46 @@
+import attr
+
 import torch
 
 from ..utils import place
+from .search import Search
 
 
-def beam_search(m, spec, k, max_steps=100):
-    weights = place(m, torch.zeros(1))
-    beams = place(m, torch.zeros(k, 0, dtype=torch.long))
-    finished = []
-    state, preds = m.begin_inference([spec])
-    for _ in range(max_steps):
-        weights = weights[:, None] + preds
-        good_finished = sum(1 for k, v in finished if k >= weights.max())
-        values, indices = torch.topk(
-            weights.flatten(), min(k - good_finished, len(weights.flatten()))
-        )
-        batch_idxs, tokens = flattened_meshgrid_like(weights)
-        batch_idxs = batch_idxs[indices]
-        tokens = tokens[indices]
-        weights = weights.flatten()[indices]
+@attr.s
+class BeamSearch(Search):
+    k = attr.ib()
+    max_steps = attr.ib(default=100)
 
-        beams = torch.cat([beams[batch_idxs], tokens[:, None]], axis=1)
+    def __call__(self, m, spec):
+        weights = place(m, torch.zeros(1))
+        beams = place(m, torch.zeros(self.k, 0, dtype=torch.long))
+        finished = []
+        state, preds = m.begin_inference([spec])
+        for _ in range(self.max_steps):
+            weights = weights[:, None] + preds
+            good_finished = sum(1 for k, v in finished if k >= weights.max())
+            values, indices = torch.topk(
+                weights.flatten(), min(self.k - good_finished, len(weights.flatten()))
+            )
+            batch_idxs, tokens = flattened_meshgrid_like(weights)
+            batch_idxs = batch_idxs[indices]
+            tokens = tokens[indices]
+            weights = weights.flatten()[indices]
 
-        dones = tokens == 1
-        if dones.all():
-            break
-        finished.extend(zip(weights[dones], beams[dones]))
-        beams = beams[~dones]
-        batch_idxs = batch_idxs[~dones]
-        tokens = tokens[~dones]
-        weights = weights[~dones]
+            beams = torch.cat([beams[batch_idxs], tokens[:, None]], axis=1)
 
-        state, preds = state.resample(batch_idxs).step(tokens)
-    finished.extend(zip(weights, beams))
-    return sorted(((w, b.tolist()) for w, b in finished), reverse=True)
+            dones = tokens == 1
+            if dones.all():
+                break
+            finished.extend(zip(weights[dones], beams[dones]))
+            beams = beams[~dones]
+            batch_idxs = batch_idxs[~dones]
+            tokens = tokens[~dones]
+            weights = weights[~dones]
+
+            state, preds = state.resample(batch_idxs).step(tokens)
+        finished.extend(zip(weights, beams))
+        return sorted(((w.item(), b.tolist()) for w, b in finished), reverse=True)
 
 
 def flattened_meshgrid_like(tensor):
---
+++ repl/search/search.py
@@ -0,0 +1,24 @@
+import torch
+
+from abc import ABC, abstractmethod
+
+from permacache import permacache, stable_hash
+
+
+class Search(ABC):
+    @abstractmethod
+    def __call__(self, model, spec):
+        pass
+
+
+def frozen_hash(m):
+    if not hasattr(m, "_frozen_hash"):
+        m._frozen_hash = stable_hash(m.state_dict())
+    return m._frozen_hash
+
+
+@permacache("repl/search", key_function=dict(m=frozen_hash, spec=stable_hash))
+def infer(search, m, spec):
+    m.eval()
+    with torch.no_grad():
+        return search(m, spec)
