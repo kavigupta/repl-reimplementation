c7596e3c134d386fd5743c83db22c6acdc62ea85
Kavi Gupta <kavi@berkeley.edu>
2021-01-03 14:16:04 -0800

    add ability to change the embedding size

--- experiments/lgrl_karel.py
+++ experiments/lgrl_karel.py
@@ -7,14 +7,15 @@ from repl.karel_environment.spec_encoder import KarelSpecEncoder
 from repl.karel_environment.load_karel_environment import KarelDataset
 from repl.train import supervised_training
 
+embedding_size = 256
 
 supervised_training(
     data=KarelDataset("train").multiple_epochs_iter(seed=0, batch_size=16, epochs=10),
-    optimizer=lambda parameters: torch.optim.Adam(parameters, lr=1e-4),
+    optimizer=lambda parameters: torch.optim.Adam(parameters, lr=1e-4 * 16 / 128),
     architectures=[
-        lambda: LGRL(KarelSpecEncoder(embedding_size=64), embedding_size=64)
+        lambda: LGRL(KarelSpecEncoder(embedding_size=embedding_size), embedding_size=embedding_size)
     ],
-    paths=["logdirs/lgrl-karel"],
+    paths=[f"logdirs/lgrl-karel-{embedding_size}"],
     save_frequency=200,
     report_frequency=20,
 )
