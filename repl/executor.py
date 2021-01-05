67326d9cf1d6b430f745ae543e7978e5cabe4669
Kavi Gupta <kavi@berkeley.edu>
2021-01-05 14:41:07 -0800

    evaluate, evaluate_on_dataset added

--- repl/executor.py
+++ repl/executor.py
@@ -36,12 +36,34 @@ def evaluate_pair(executor, program, pair):
             return "runtime-error"
 
 
-TABLE_KEYS = "correct", "incorrect", "syntax-error", "runtime-error", "total"
+ERROR_KEYS = "syntax-error", "runtime-error", "incorrect"
+TABLE_KEYS = "correct", *ERROR_KEYS, "total"
 
 
-def evaluate(executor, program, spec):
+def evaluate(executor, program, spec, *, use_test):
     outcome = {k: 0 for k in TABLE_KEYS}
-    for pair in spec.pairs:
+    outcome["each"] = []
+    pairs = spec.pairs
+    if use_test:
+        pairs = pairs + spec.test_pairs
+    for pair in pairs:
         outcome["total"] += 1
-        outcome[evaluate_pair(executor, program, pair)] += 1
+        res = evaluate_pair(executor, program, pair)
+        outcome[res] += 1
+        outcome["each"] += [res]
     return outcome
+
+
+def evaluate_on_dataset(executor, specs, outputs, pbar=lambda x: x, **kwargs):
+    results = {k: 0 for k in TABLE_KEYS}
+    for output, spec in pbar(list(zip(outputs, specs))):
+        _, program = output[0]
+        outcome = evaluate(executor, program, spec, **kwargs)
+        results["total"] += 1
+        for err in ERROR_KEYS:
+            if outcome[err] > 0:
+                results[err] += 1
+                break
+        else:
+            results["correct"] += 1
+    return results
