7482e48fa950a9f92ed54e0c3020e1216a65dc62
Kavi Gupta <kavi@berkeley.edu>
2021-02-09 16:13:05 -0800

    fix issues with the dynamics model

--- synthesis/environment/karel/sequential_dynamics.py
+++ synthesis/environment/karel/sequential_dynamics.py
@@ -1,6 +1,8 @@
 import attr
 
-from karel_for_synthesis import execute
+import numpy as np
+
+from karel_for_synthesis import execute, ExecutorRuntimeException
 
 from ...repl.dynamics import Dynamics
 from ...repl.program import SequentialProgram
@@ -14,11 +16,16 @@ class KarelDynamics(Dynamics):
         return super().partially_execute(program, spec)
 
     def partially_execute_pair(self, program, pair):
-        return execute(toks_to_program(program.tokens), pair.input).result
+        try:
+            return execute(toks_to_program(program.tokens), pair.input).result
+        except ExecutorRuntimeException:
+            return pair.input
 
     def program_is_correct(self, program, spec):
         executed = self.partially_execute(program, spec)
-        return [np.all(e) == pair.output for e, pair in zip(executed, spec.pairs)]
+        return np.all(
+            [np.all(e == pair.output) for e, pair in zip(executed, spec.pairs)]
+        )
 
     def program_is_complete(self, program, spec):
         return len(program.tokens) == self.size
